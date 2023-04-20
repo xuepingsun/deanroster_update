3f01f9e6561fdbea0f1e5138b6aec3ae27539b5d
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-19 22:13:07 -0700

    fix typo

--- roster/admin.py
+++ roster/admin.py
@@ -5,7 +5,7 @@ from django.forms import TextInput, Textarea
 
 
 # Register your models here.
-from .models import XiInstitute， DeanBasic, DeanID,DeanCV,Deanedu#,UserVisit #Post,UserInput,
+from .models import XiInstitute,DeanBasic, DeanID,DeanCV,Deanedu#,UserVisit #Post,UserInput,
 # from .models import DeanInfo
 
 # admin.site.register(Post)
