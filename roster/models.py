1b1e5396782ba9d83d3e940701d2d52d408e2231
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-24 10:37:43 -0700

    Update models.py

--- roster/models.py
+++ roster/models.py
@@ -182,7 +182,11 @@ class DeanCV(models.Model):
     # job_content= models.TextField()
 
     job_title= models.CharField(max_length=20)
-    # job_location= models.CharField(max_length=20)
+    job_title_level= models.CharField(max_length=20,
+            choices=[("dean","院长"),("vice-dean","副院长"),
+                    ("prof","教授"),("associate-prof","副教授")
+                    ,("assistant-prof","助理教授/讲师"),
+                    ,("postdoc","博士后")])
     job_country= models.CharField(max_length=10)
     job_institution=models.CharField(max_length=20)
 
