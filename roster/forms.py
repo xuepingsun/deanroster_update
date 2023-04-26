b5c8f741ef12cb9da3aaefa2de4d73b4d3766e47
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-26 07:54:53 -0700

    remove the variable category from deanbasic form

--- roster/forms.py
+++ roster/forms.py
@@ -7,10 +7,10 @@ from .models import DeanBasic, SchoolInfo#UserInput,
 #         fields = ['university','school','name', 'st_year_mon','end_year_mon', 'note']
 
 class DeanBasicForm(forms.ModelForm):
-    category=forms.ModelChoiceField(
-        queryset=SchoolInfo.objects.all(),
-        widget=forms.Select()
-    )
+    # category=forms.ModelChoiceField(
+    #     queryset=SchoolInfo.objects.all(),
+    #     widget=forms.Select()
+    # )
     class Meta:
         model = DeanBasic
         fields = "__all__" #'university','school','name', 'st_year_mon','end_year_mon', 'CV_string']
