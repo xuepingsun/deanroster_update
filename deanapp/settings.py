b000d30bc4079ad2aafbc63c7716a6ced722a4d7
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-26 07:22:13 -0700

    Update settings.py

--- deanapp/settings.py
+++ deanapp/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-        os.path.join(BASE_DIR, 'roster', 'templates')
+        BASE_DIR.joinpath( 'roster', 'templates')
         ],
         "APP_DIRS": True,
         "OPTIONS": {
