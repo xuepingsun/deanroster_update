384285c84ef152019dea115519508bcdb5d8e03f
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-26 07:19:29 -0700

    Update settings.py

--- deanapp/settings.py
+++ deanapp/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = "deanapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+        os.path.join(BASE_DIR, 'roster', 'templates')
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
