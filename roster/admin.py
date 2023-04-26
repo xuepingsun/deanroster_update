d66c66b0d52f4c047d83ee6eebd1b34775762e3e
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-26 08:36:41 -0700

    Update admin.py

--- roster/admin.py
+++ roster/admin.py
@@ -63,9 +63,9 @@ class SchoolInfoAdmin(NestedModelAdmin,admin.ModelAdmin):
 
     # list_display = ['university','school', 'num_deans']#'university_school',
     #
-    # def num_deans(self, obj):
-    #     return obj.deanbasic_set.count()
-    # num_deans.short_description = 'Number of Deans'
+    def num_deans(self, obj):
+        return obj.deanbasic_set.count()
+    num_deans.short_description = 'Number of Deans'
     #
     # def get_queryset(self, request):
     #     queryset = super().get_queryset(request)
@@ -77,29 +77,70 @@ class SchoolInfoAdmin(NestedModelAdmin,admin.ModelAdmin):
     #
 
 
+# admin.py
+# from django.contrib import admin
+# from django.contrib.contenttypes.models import ContentType
+# from django.db import models
+from django.template.response import TemplateResponse
 
+# from .models import SchoolInfo, DeanBasic
+
+
+# class SchoolInfoAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'num_deans')
+#     ordering = ('name',)
+#
+#     def num_deans(self, obj):
+#         return obj.deanbasic_set.count()
+#
+#     num_deans.short_description = 'Number of Deans'
+
+
+class SchoolInfoDashboard(admin.AdminSite):
+    site_header = f"{get_model_info(SchoolInfo)[1].title()} Administration"
+    site_title = f"{get_model_info(SchoolInfo)[1].title()} Admin Portal"
+    index_title = f"{get_model_info(SchoolInfo)[1].title()} Dashboard"
+    index_template = 'admin/admin_index.html'
+
+    def get_urls(self):
+        from django.urls import path
+        urls = super().get_urls()
+        urls += [
+            path('', self.admin_view(self.dashboard_view), name='index'),
+        ]
+        return urls
+
+    def dashboard_view(self, request):
+        schools = SchoolInfo.objects.annotate(num_deans=models.Count('deanbasic'))
+        context = {
+            'schools': schools,
+        }
+        return TemplateResponse(request, self.index_template, context)
+
+
+admin_site = SchoolInfoDashboard(name='schoolinfoadmin')
 
 
 admin.site.register(SchoolInfo,SchoolInfoAdmin)
 admin.site.register(DeanBasic,DeanBasicAdmin)
 admin.site.register(XiInstitute)
 
-admin.site.index_template = 'admin/admin_index.html'
-def get_schools_with_dean_counts():
-    schools = SchoolInfo.objects.annotate(num_deans=models.Count('deanbasic'))
-    return schools
-
-def get_model_info(model):
-    content_type = ContentType.objects.get_for_model(model)
-    app_label = content_type.app_label
-    model_name = content_type.model
-    return app_label, model_name
-
-@admin.display(description='Number of Deans')
-def num_deans(self, obj):
-    return obj.num_deans
-
-admin.site.site_header = f"{get_model_info(SchoolInfo)[1].title()} Administration"
-admin.site.site_title = f"{get_model_info(SchoolInfo)[1].title()} Admin Portal"
-admin.site.index_title = f"{get_model_info(SchoolInfo)[1].title()} Dashboard"
-admin.site.schools = get_schools_with_dean_counts()
+# admin.site.index_template = 'admin/admin_index.html'
+# def get_schools_with_dean_counts():
+#     schools = SchoolInfo.objects.annotate(num_deans=models.Count('deanbasic'))
+#     return schools
+#
+# def get_model_info(model):
+#     content_type = ContentType.objects.get_for_model(model)
+#     app_label = content_type.app_label
+#     model_name = content_type.model
+#     return app_label, model_name
+#
+# @admin.display(description='Number of Deans')
+# def num_deans(self, obj):
+#     return obj.num_deans
+#
+# admin.site.site_header = f"{get_model_info(SchoolInfo)[1].title()} Administration"
+# admin.site.site_title = f"{get_model_info(SchoolInfo)[1].title()} Admin Portal"
+# admin.site.index_title = f"{get_model_info(SchoolInfo)[1].title()} Dashboard"
+# admin.site.schools = get_schools_with_dean_counts()
