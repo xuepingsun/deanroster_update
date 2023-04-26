ead2ecd92a944cf9989ebdf339fd8f8e49d619cf
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-26 07:44:08 -0700

    change urls.py

--- deanapp/urls.py
+++ deanapp/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from roster import views
+# from roster import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.admin_index, name='admin_index'),
     path('', include('roster.urls')),
 ]
