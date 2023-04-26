237f61af4357fc0a73bf346ca05f157b295bc7c5
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-26 07:42:37 -0700

    Update urls.py

--- deanapp/urls.py
+++ deanapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .roster import views
+from roster import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
