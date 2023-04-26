378cbdb3a3fb6629d3621afcb2da13543f2fe0ec
Xueping Sun <70743963+xuepingsun@users.noreply.github.com>
2023-04-26 04:02:26 -0700

    Update admin.py

--- roster/admin.py
+++ roster/admin.py
@@ -35,7 +35,7 @@ class DeanBasicAdmin(NestedModelAdmin,admin.ModelAdmin):
     #     )
     inlines = [DeanIDInline,DeanCVInline,DeaneduInline]
     form=DeanBasicForm
-    change_form_template='adm/deanbasic_university_school_form.html'
+    change_form_template='admin/deanbasic_university_school_form.html'
 
 # @admin.site.register(DeanBasicAdmin)
 # class AllModelAdmin(admin.ModelAdmin):
